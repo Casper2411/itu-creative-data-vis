01df50fe5f529f95f2c37c680fa8d3d70770dbcd
Casper Frøding <ccaassppeerr2411@gmail.com>
2024-07-18 13:11:39 +0200

    added something to show max and min

--- w2/RainDrops_project/index.js
+++ w2/RainDrops_project/index.js
@@ -73,12 +73,19 @@ function makeRainLines(g, startX, startY, previousAngle, num, Data){
 
 	// Convert angle from degrees to radians
     var angleRad = (Data.data[num]+previousAngle) * Math.PI / 180;
-	var length = num*(h/20);
+	var length = num*(h/18);
 
     // Calculate endpoint coordinates
     var endX = startX + length * Math.cos(angleRad);
     var endY = startY + length * Math.sin(angleRad);
 
+	g.append("line")
+		.attr("x1", startX)
+		.attr("y1", startY)
+		.attr("x2", endX)
+		.attr("y2", endY)
+		.attr("stroke", "transparent")
+		.attr("stroke-width", 10)
 	g.append("line")
 		.attr("x1", startX)
 		.attr("y1", startY)
@@ -119,7 +126,7 @@ function makeRainLines(g, startX, startY, previousAngle, num, Data){
 			.attr("y", endY)
 			.text(`${Data.data[num]}💧`) //°
 			.style("fill", "white")
-			.style("font-size", "12px")
+			.style("font-size", "15px")
 			.style("fill", "yellow")
 			.style("text-shadow", "6px 6px 12px rgba(0, 0, 0, 1)") // Add shadow for better visibility
 			.style("display", "none") // Hide the text initially
@@ -129,9 +136,13 @@ function makeRainLines(g, startX, startY, previousAngle, num, Data){
 	makeRainLines(g, endX,endY,(Data.data[num]+previousAngle), num+1, Data);
 }
 
+var minRainName = d3.min(rainDrops, d => d.data[d.data.length-1])
+var maxRainName = d3.max(rainDrops, d => d.data[d.data.length-1])
 rainDrops.forEach(function(d, i) {
+	console.log("min" + minRainName)
+	console.log("max" + maxRainName)
     var g = svg.append("g")
-               .attr("transform", `translate(${w / 2},${h / 6})`);
+               .attr("transform", `translate(${0},${0})`);
 	// Add event listeners to the group
     g.on("mouseover", function() {
         // Fade out all groups except the hovered one
@@ -150,14 +161,57 @@ rainDrops.forEach(function(d, i) {
            .style("display", "none"); // Hide text on mouseout
     });
 
-    makeRainLines(g, 50, 50, Math.PI / 4, 0, d);
-
+    makeRainLines(g, w/2 + 100, h/4, Math.PI / 4, 0, d);
+	
 	g.append("text")
-		.attr("x", w/3)
-		.attr("y", h-100 - (i*25))
+		.attr("x", w-125)
+		.attr("y", h-50 - (i*30))
 		.text(`${d.name}`)
 		.style("fill", function(){
 			return colorScale(d.name);
 		})
-		.style("font-size", "20px")
-}).then();
+		.style("font-size", "25px")
+	//add button
+	if (d.data[d.data.length-1] === minRainName){
+		g.append("text")
+			.text("Person with least amount of rain observed")
+			.attr("x", 50)
+			.attr("y", 350)
+			.style("fill", "CornflowerBlue")
+			.style("text-decoration", "underline")
+	}
+	if (d.data[d.data.length-1] === maxRainName){
+		g.append("text")
+			.text("Person with the most amount of rain observed")
+			.attr("x", 50)
+			.attr("y", 375)
+			.style("fill", "CornflowerBlue")
+			.style("text-decoration", "underline")
+	}
+
+
+});
+
+//Title
+svg.append("text")
+   .attr("x", 50)
+   .attr("y", 65)
+   .style("fill", "white")
+   .text("Rain oberservations visualised")
+   .style("font-size", "35px")
+
+//Legend
+svg.append("text")
+   .attr("x", 50)
+   .attr("y", 100)
+   .style("fill", "white")
+   .selectAll("tspan")
+   .data(["This data shows the observations", "of amount of raindrops landing on", 
+   			"a persons face, depending on how many steps.", " ", "The length of the line is amount",
+			"of steps, ranging from 10-60, and", "the angle is the amount of raindrops.",
+			"You can try to hover over either the lines,", "or the names to see more details."])
+   .enter()
+   .append("tspan")
+   .attr("x", 50)
+   .attr("dy", (d, i) => (i === 0 ? 0 : 23)) // Adjust the spacing as needed
+   .text(d => d);
